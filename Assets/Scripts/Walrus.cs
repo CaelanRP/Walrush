8cffdcb8374999dc6f6810282d60259f47d236a2
Caelan <caelan@aggrocrab.com>
2022-06-27 12:13:05 +0200

    Basic waddle movement

--- Assets/Scripts/Walrus.cs
+++ Assets/Scripts/Walrus.cs
@@ -6,6 +6,8 @@ using Rewired;
 public class Walrus : Entity
 {
     public float waddleSpeed;
+    public float minRotateSpeed, maxRotateSpeed, rotateIncreaseRate, minAngularDrag, maxAngularDrag;
+    float currentRotateForce;
     Rewired.Player _i;
     
     public Rewired.Player input{
@@ -28,12 +30,29 @@ public class Walrus : Entity
     void HandleInputFixed(){
         currentWalkVector = input.GetAxis2D("MoveX", "MoveY");
         
-        cameraRelativeMoveVector = CameraController.Instance.transform.TransformVector(currentWalkVector);
+        cameraRelativeMoveVector = CameraController.Instance.transform.TransformVector(new Vector3(currentWalkVector.x, 0, currentWalkVector.y));
 
         rb.AddForce(cameraRelativeMoveVector * waddleSpeed);
-        //currentMoveForce = new Vector3()
 
-
+        if (currentWalkVector.magnitude > 0.1f){
+            ApplyWalkTorque();
+        }
+        else{
+            currentRotateForce = Mathf.Max(currentRotateForce - rotateIncreaseRate * Time.fixedDeltaTime, maxRotateSpeed);
+        }
         //rb.AddForce(new Vector2)
+    }
+
+    void ApplyWalkTorque(){
+        float angle = Vector3.SignedAngle(transform.forward, cameraRelativeMoveVector, Vector3.up);
+        float unsignedAngle = Mathf.Abs(angle);
+
+        if (unsignedAngle > 5){
+
+            currentRotateForce = Mathf.Min(currentRotateForce + rotateIncreaseRate * Time.fixedDeltaTime, maxRotateSpeed);
+            rb.AddTorque(Vector3.up * Mathf.Sign(angle) * currentRotateForce);
+        }
+
+        rb.angularDrag = Mathf.Lerp(maxAngularDrag, minAngularDrag, unsignedAngle / 180);
     }
 }
